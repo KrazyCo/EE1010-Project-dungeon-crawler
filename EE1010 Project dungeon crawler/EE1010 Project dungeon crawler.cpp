8bc967692a090ec8edaea0ef129fc9d3d334c03c
KrazyCo <conorstrickland@hotmail.com>
2025-01-13 00:18:32 +0000

    bug fix and readme small update

--- EE1010 Project dungeon crawler/EE1010 Project dungeon crawler.cpp
+++ EE1010 Project dungeon crawler/EE1010 Project dungeon crawler.cpp
@@ -92,7 +92,22 @@ int main()
 				else if (userInput == "help")
 				{
 					// explain game to user
-					std::cout << "blah blah\n";
+					std::cout << "\n"
+						"                        Welcome to Dungeons, where you have to defeat the final monster to win!\n"
+						"                        While playing the game, you have a few options of choices you can make,\n"
+						"                        these are shown in brackets().\n\n"
+						"                        You can find items (swords and shields) around the dungeon to help you\n"
+						"                        fight monsters, they will be required for some of them!\n"
+						"                        Stronger items are protected by monsters, and will only be shown after\n"
+						"                        they are defeated!\n\n"
+						"                        When you defeat a monster, you will make a health potion and heal up\n"
+						"                        slightly depending on how strong the monster was, \n"
+						"                        so if you are low health you can go kill monsters to heal up enough.\n\n"
+						"                        The room to the final monster is locked by a gate, so you will need\n"
+						"                        to find the key in the dungeon to be able to enter the final room.\n\n"
+						"                        If there a monster in a room, you will not be able to pass\n"
+						"                        until the monster has been defeated, although you can retreat\n"
+						"                        from the way you came if needed\n";
 				}
 				else if (userInput == "exit")
 				{
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # EE1010 Project dungeon crawler
+
+A demo of a dungeon crawler game for my uni module
